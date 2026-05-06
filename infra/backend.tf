eed35b5fa5559d1f6b596bd344f4f3f5f355709c
Diego Ayala <diego@MacBook-Air.local>
2026-05-05 21:38:10 -0300

    add state

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "dha-personal-site-tfstate"
-    key            = "personal-site/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "dha-personal-site-tfstate-lock"
-    encrypt        = true
+    bucket       = "dha-personal-site-tfstate"
+    key          = "personal-site/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
